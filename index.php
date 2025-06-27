833a2ada585773eedeb8f6cd6c8da2e09e14760a
ZehtX <kp1703233@gmail.com>
2025-06-27 18:54:13 -0400

    Test

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['email'])){ 	//Agregar la direccion "/Login/login.php" para el hosting
-    header("location: Login/login.php");
-}else{
-    header("location: Dashboard");
-}
-?>
